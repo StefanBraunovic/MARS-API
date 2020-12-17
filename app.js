f25d9a3c75306b2d83ef7d19fcd58ca2692cfed2
Stefan Braunovic <stefan.braunovic55@gmail.com>
2020-12-17 12:41:58 +0100

    lazy loading

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
 
 
-
-const galerija = document.getElementById('galerija');
-// fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=KhjE42rF9Y9gIE1JW1mcBX7RACCuQrwHPxczOnuj')
-//   .then(function(response) {
-//     return response.json()
-//   }).then(function(response){
-//       console.log(response);
-//       const slike = response.photos;
-//     slike.forEach(slika=>{  
-//         galerija.innerHTML +=`<div>
-                
-//         <img id='slika' src="${slika.img_src}">
-//         <h3>camera name: ${slika.camera.name}</h3>
-//         </div>
-//         `
-// })
-// });
-
-async function getImage(){
-    const response = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=KhjE42rF9Y9gIE1JW1mcBX7RACCuQrwHPxczOnuj');
-    const img_data = await response.json();
-    
-    const slike = img_data.photos;
-  slike.forEach(slika=>{  
-        galerija.innerHTML +=`<div>
-                
-        <img id='slika' src="${slika.img_src}">
-        <h3>camera name: ${slika.camera.name}</h3>
-        </div>
-        `
-})
-
-}
+const URL ="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=KhjE42rF9Y9gIE1JW1mcBX7RACCuQrwHPxczOnuj";
+      document.addEventListener("DOMContentLoaded", () => {
+        //set up the IntersectionObserver to load more images if the footer is visible.
+        let options = {
+          root: null,
+          rootMargins: "0px",
+          threshold: 0.5
+        };
+        const observer = new IntersectionObserver(handleIntersect, options);
+        observer.observe(document.querySelector("footer"));
+        //an initial load of some data
+        getData();
+      });
+      function handleIntersect(entries) {
+        if (entries[0].isIntersecting) {
+          console.warn("something is intersecting with the viewport");
+          getData();
+        }
+      }
 
 
-getImage()
+      function getData() {
+        let main = document.querySelector("main");
+        console.log("fetch some JSON data");
+        fetch(URL)
+          .then(response => response.json())
+          .then(function(response){
+                  console.log(response);
+                  const slike = response.photos;
+                  console.log(slike);
+                slike.forEach(slika=>{  
+                  let img = document.createElement("img");
+                  img.src=slika.img_src;
+                  main.appendChild(img)
+            })
+       
+      })}
+
+
+
 
 
 
